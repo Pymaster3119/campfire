da274a99b289ee137081091d276c5360cd6c523a
Aditya Anand <aditya.anand3119@gmail.com>
2026-02-28 13:19:46 -0500

    complete player controller

--- HankMovement.cs
+++ HankMovement.cs
@@ -3,38 +3,49 @@ using System;
 
 public partial class HankMovement : CharacterBody2D
 {
-	public const float Speed = 300.0f;
-	public const float JumpVelocity = -400.0f;
+	[Export]
+	public float Speed { get; set; } = 300.0f;
+
+	private Vector2 _targetPosition = Vector2.Zero;
+
+	public override void _Ready()
+	{
+		_targetPosition = GlobalPosition;
+	}
+
+	public override void _Input(InputEvent @event)
+	{
+		if (@event.IsActionPressed("click"))
+		{
+			_targetPosition = GetGlobalMousePosition();
+		}
+	}
 
 	public override void _PhysicsProcess(double delta)
 	{
-		Vector2 velocity = Velocity;
-
-		// Add the gravity.
-		if (!IsOnFloor())
+		Vector2 direction;
+		if (ladder())
 		{
-			velocity += GetGravity() * (float)delta;
-		}
-
-		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-		{
-			velocity.Y = JumpVelocity;
-		}
-
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		if (direction != Vector2.Zero)
-		{
-			velocity.X = direction.X * Speed;
+			direction = new Vector2(0, 1) * GlobalPosition.DirectionTo(_targetPosition);
 		}
 		else
 		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			direction = new Vector2(1, 0) * GlobalPosition.DirectionTo(_targetPosition);
 		}
 
-		Velocity = velocity;
+		if (GlobalPosition.DistanceTo(_targetPosition) < 5.0f)
+		{
+			Velocity = Vector2.Zero;
+		}
+		else
+		{
+			Velocity = direction * Speed;
+		}
 		MoveAndSlide();
+	}
+	public bool ladder()
+	{
+		//TODO: Impliment this thing
+		return false;
 	}
 }
