0bd8ca5389a109ec8dc82bc1640453a5b7532175
Aditya Anand <aditya.anand3119@gmail.com>
2026-02-28 14:24:22 -0500

    Hanky my love

--- HankMovement.cs
+++ HankMovement.cs
@@ -6,6 +6,8 @@ public partial class HankMovement : CharacterBody2D
 	[Export]
 	public float Speed { get; set; } = 300.0f;
 	public static Vector2 hankPosition;
+	[Export]
+	public TileMap scene;
 
 	private Vector2 _targetPosition = Vector2.Zero;
 
@@ -48,7 +50,26 @@ public partial class HankMovement : CharacterBody2D
 	}
 	public bool ladder()
 	{
-		//TODO: Impliment this thing
+		scene = GetTree().CurrentScene.GetNode<TileMap>("TileMap");
+		Vector2 localPosition = scene.ToLocal(GlobalPosition);
+		Vector2I mapCoords = scene.LocalToMap(localPosition);
+		int layer = 0; // Assuming layer 0, adjust as needed.
+		int sourceId = scene.GetCellSourceId(layer, mapCoords);
+		Vector2I atlasCoords = scene.GetCellAtlasCoords(layer, mapCoords);
+		int alternativeTile = scene.GetCellAlternativeTile(layer, mapCoords);
+		TileData tileData = scene.GetCellTileData(layer, mapCoords);
+
+		if (sourceId != -1)
+		{
+			if (atlasCoords[0] <=3)
+			{
+				return true;
+			}
+		}
+		else
+		{
+			GD.Print($"No tile found at map coordinates: {mapCoords}");
+		}
 		return false;
 	}
 }
