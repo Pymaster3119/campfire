36102c719f5f4875179af29ededcb43815bcfc89
Aditya Anand <aditya.anand3119@gmail.com>
2026-03-01 10:22:02 -0500

    colors of bars

--- AttributeBar.cs
+++ AttributeBar.cs
@@ -9,11 +9,35 @@ public partial class AttributeBar : ProgressBar
 	public override void _Ready()
 	{
 	}
+	
+	public void SetFillColor(Color newColor)
+	{
+		StyleBoxFlat fillStyleBox = GetThemeStylebox("fill") as StyleBoxFlat;
 
+		if (fillStyleBox != null)
+		{
+			StyleBoxFlat uniqueFillStyleBox = (StyleBoxFlat)fillStyleBox.Duplicate();
+			uniqueFillStyleBox.BgColor = newColor;
+			AddThemeStyleboxOverride("fill", uniqueFillStyleBox);
+		}
+	}
+	
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 		Value = HankStats.StatLevel(attribute);
 		ProgressBarText.Text = attribute;
+		if (Value < 20.0f)
+		{
+			SetFillColor(Colors.Red);
+		}
+		else if (Value < 50.0f)
+		{
+			SetFillColor(Colors.Yellow);
+		}
+		else
+		{
+			SetFillColor(Colors.Green);
+		}
 	}
 }
